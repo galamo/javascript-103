d1b61870630af56129cf9e41ffaf800a65152afb
galamo <galamouyal88@gmail.com>
2024-03-06 21:26:40 +0200

    a

---
+++ lesson_1/page27.js
@@ -0,0 +1,10 @@
+console.log("=========================ex1=========================")
+var topNumber = Number(prompt("Enter number top"))
+var index = 1;
+while (index <= topNumber) {
+    console.log("The next number is: " + index)
+    index = index + 1;
+    // index += 1
+}
+console.log("Finished")
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Homework 6.3
+- page 18 ex 5 (js)
+- page 20 ex 4 (js)
+- Page 27 (js+diagram)
+ex 2 , ex 3, Bonus: ex 5
