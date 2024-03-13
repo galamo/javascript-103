6117493e40e3ceaf444cafbade6cc34eade0c7ca
galamo <galamouyal88@gmail.com>
2024-03-13 21:24:43 +0200

    a

--- lesson_2/page46.js
+++ lesson_2/page46.js
@@ -1,14 +1,21 @@
 function getClock(hours, min, sec) {
-    if (hours < 10) {
-        hours = "0" + hours
+    if (hours < 24 && hours >= 0 && min >= 0 && min < 60 && sec >= 0 && sec < 60) {
+        if (hours < 10) {
+            hours = "0" + hours
+        }
+        if (min < 10) {
+            min = "0" + min
+        }
+        if (sec < 10) {
+            sec = "0" + sec
+        }
+        return hours + ":" + min + ":" + sec
+    } else {
+        return "Wrong input"
     }
-    if (min < 10) {
-        min = "0" + min
-    }
-    if (sec < 10) {
-        sec = "0" + sec
-    }
-    return hours + ":" + min + ":" + sec
-
-
 }
+
+console.log(getClock(12, 20, 55))
+console.log(getClock(5, 20, 12))
+console.log(getClock(6, 2, 55))
+console.log(getClock(8, 1, 55))
