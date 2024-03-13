9c9a7457799a6139c8d943bc8a3fa1763c213b5c
galamo <galamouyal88@gmail.com>
2024-03-13 20:58:31 +0200

    a

--- lesson_2/page35.js
+++ lesson_2/page35.js
@@ -6,9 +6,33 @@
 // }
 
 // ex 3
-var nm = +prompt("Enter the number to get n! 1*2*3....n")
-var result = 1;
-for (let index = 1; index <= nm; index++) {
-    result = result * index
+// var nm = +prompt("Enter the number to get n! 1*2*3....n")
+// var result = 1;
+// for (let index = 1; index <= nm; index++) {
+//     result = result * index
+// }
+// console.log(result)
+
+// ex 4
+// var num = +prompt("Enter some number")
+// var sum = 0;
+// for (let index = 0; index <= num; index++) {
+//     if (index % 3 === 0) {
+//         sum = sum + index
+//     }
+// }
+// console.log(sum)
+
+
+// ex 6
+
+var num = +prompt("Enter some number")
+var sum = 0;
+for (let index = 0; index <= num; index++) {
+    sum = sum + Math.pow(2, index)
 }
-console.log(result)
+console.log(sum)
+
+
+
+
