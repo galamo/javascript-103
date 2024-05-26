2510ed3e18b16b202a679eddbae54f219224e150
galamo <galamouyal88@gmail.com>
2024-05-26 18:42:05 +0300

    a

--- lesson_17/jquery/index.js
+++ lesson_17/jquery/index.js
@@ -1,9 +1,15 @@
+const svgDrag = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grip-vertical" viewBox="0 0 16 16">
+<path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+</svg>
+`
 $(document).ready(function () {
     const sortableTaskList = $("#vacationsTaskList")
     sortableTaskList.sortable();
     $("#addTaskAction").on("click", function () {
         const taskDescription = $("#task").val()
-        const listItemString = `<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>${taskDescription}</li>`
+        const taskImageUrl = $("#image").val()
+        const imageItemString = `<img src="${taskImageUrl}" alt="" height="50" width="50">`
+        const listItemString = `<li class="ui-state-default">${svgDrag}${taskDescription} ${imageItemString}</li>`
         sortableTaskList.append(listItemString)
     })
 });
---
+++ lesson_17/jquery_api/index.js
@@ -0,0 +1,39 @@
+
+$(document).ready(async function () {
+
+    const loader = $("#loader")
+
+    try {
+        const data = await getJokesApi()
+        const jokesUi = getJokesUI(data);
+        $("#jokesList").append(jokesUi)
+        $("#jokesList").on("click", async function (event) {
+            if (!event.target.id) return;
+            const t = getJokeById(event.target.id)
+        })
+    } catch (error) {
+        alert("Something went wrong!")
+    } finally {
+        loader.toggle()
+    }
+
+    function getJokesUI(data) {
+        const elements = data.map((singleJoke) => {
+            return `<div class="i-am-clickable" id="${singleJoke.id}">[${singleJoke.id}] ${singleJoke.setup} </div>`
+        })
+        return elements
+    }
+
+});
+
+async function getJokesApi() {
+    const result = await fetch("https://official-joke-api.appspot.com/jokes/ten")
+    const data = await result.json();
+    return data;
+}
+
+async function getJokeById(id) {
+    const result = await fetch(`https://official-joke-api.appspot.com/jokes/${id}`)
+    const data = await result.json();
+    return data;
+}
