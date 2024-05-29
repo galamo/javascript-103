801f74f5c78cf0c89aba51551345ba3655f89fdc
galamo <galamouyal88@gmail.com>
2024-05-29 20:28:30 +0300

    a

--- lesson_19_complexity/index.js
+++ lesson_19_complexity/index.js
@@ -10,10 +10,35 @@
 
 const queue = []
 function enqueue(item) { // insert into queue
-    queue.push(item)
+    queue.push(item) // O(1)
 }
 function dequeue() { // remove from queue
     const theFirstInQueue = queue[0] //O(1)
     queue.splice(0, 1) // O(n)
     return theFirstInQueue // O(1)
 }
+
+
+let queue2 = {}
+let first = 0;
+let last = 0;
+function enqueue2(item) { // insert into queue
+    queue2[last] = item; // O(1)
+    last++; // O(1)
+}
+function dequeue2() { // remove from queue
+    const aniBator = queue2[first] // O(1)
+    delete queue2[first] // O(1)
+    first++ // O(1)
+    if (Object.keys(queue2).length === 0) { // O(n)
+        resetQueue()
+    }
+    return aniBator; // O(1)
+}
+
+function resetQueue() {
+    queue2 = {}
+    first = 0;
+    last = 0;
+}
+
